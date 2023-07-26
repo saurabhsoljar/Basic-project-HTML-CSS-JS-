69af6000b3f9b87166b53ad375634a4cde250c86
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-07-26 22:44:37 +0530

    single page apli

---
+++ Ecomourse/store/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+
+    white: '#ffffff'
+    Orange:{
+      '#fff7ed',
+      '#ffedd5',
+      '#fed7aa',
+      '#fdba74,'
+  },
+
+    
+    extend: {},
+  },
+  plugins: [],
+}
+
