903b3277549a2b1814b212d5a4ace8287d0c8e00
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-07-28 00:10:07 +0530

    add

---
+++ weather app/script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const  search = document.querySelector('.search-box button');
+const weather = document.querySelector('.weather-box');
+const weatherdetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.error-found');
+
+
