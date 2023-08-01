1c40f2d6644f0eb64320644c9ac91e64725b6bfe
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-08-01 23:54:01 +0530

    some update

--- weather app/script.js
+++ weather app/script.js
@@ -16,7 +16,7 @@ async function checkWeather(city){
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%" ;
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-    if(data.weather[0].main=="Clouds"){
+    if(data.weather[0].main=="clouds"){
         weatherIcon.src = "images/clouds.png";
     }
     else if(data.weather[0].main=="Clear"){
