fea257e7b1b56f22eb1cd43a877914fc6e6fbc0f
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-09-06 01:41:02 +0530

    Add section 1 front page

---
+++ Starbucks Landing Page/scrtpt.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.Starbucks').src = anything;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
