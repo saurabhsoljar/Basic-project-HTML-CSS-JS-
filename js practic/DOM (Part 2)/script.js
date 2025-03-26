d168840996aeac63ad83022507f856ad893a1a76
saurabh kumar <saurabhslite2@gmail.com>
2025-03-27 00:13:48 +0530

    dom part 2

---
+++ js practic/DOM (Part 2)/script.js
@@ -0,0 +1,39 @@
+// let div = document.querySelector("div")
+// console.log(div);
+//console.dir(div);
+
+// let id = document.getElementById("box")
+// console.log(id);
+
+// let name = div.getAttribute("name")
+// console.log(name);
+
+// let div = document.querySelector("div")
+
+// div.style.backgroundColor = "green"
+// div.style.backgroundColor = "purple"
+
+// div.style.fontSize = "20px"
+// div.innerText = "hello"
+//div.style.visibility = "hidden"
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click me"
+// console.log(newBtn);
+
+// create button using  js
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click me"
+
+// newBtn.style.color = "white"
+// newBtn.style.backgroundColor = "black"
+// newBtn.style.cursor = "pointer"
+// newBtn.onclick = function(){
+//     alert("htt")
+// }
+
+// document.querySelector("body").append(newBtn)
+
+
+
+
